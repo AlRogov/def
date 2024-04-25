9814adb8c672d23905b7a0d74185fda5a474fde3
AlRogov <166138869+AlRogov@users.noreply.github.com>
2024-04-25 16:29:06 +0300

    Add files via upload

---
+++ def.py
@@ -0,0 +1,14 @@
+def allegrova(name):
+    print('Привет,', name)
+
+
+allegrova('Андрей')
+allegrova('Андрей')
+
+
+def allegrova_2(song):
+    print(song)
+
+
+allegrova_2('Ну что же ты стоишь,')
+allegrova_2('Ну обними меня скорей!')
